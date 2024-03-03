3a787afcf08d4376e532085b6a2227e05b26bcf0
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-03-03 18:34:21 +0500

    added new data

---
+++ src/components/data/other.game.js
@@ -0,0 +1,37 @@
+export const OtherGame = [
+  {
+    id: 1,
+    title_game1: `Funny Questions`,
+    desc1: `
+1) Why did the tomato turn red?
+2) What's brown and sticky?
+3) What's orange and sounds like a parrot?
+4) What did one plate say to the other plate?
+5) What do you call a bear with no teeth?
+`,
+  },
+  {
+    id: 2,
+    title_game1: `Describe Your Best Friend`,
+    desc1: `
+"Describe Your Best Friend" is a simple game where each player takes a turn describing their own best friend
+    `,
+  },
+  {
+    id: 3,
+    title_game1: `Describe Your Self`,
+    desc1: `
+  "Describe Yourself" is a reflective activity where players articulate characteristics, interests, and experiences that define who they are as individuals. Each player takes turns sharing insights about themselves, such as personality traits, hobbies, aspirations, and memorable moments from their lives. The goal is to provide others with a deeper understanding of oneself, fostering connection and empathy within the group. It's an opportunity for self-expression and self-discovery, encouraging players to embrace their uniqueness and celebrate their personal journey.
+    `,
+  },
+  {
+    id: 4,
+    title_game1: `Describe Your Best English Teacher`,
+    desc1: `"Describe Your Best English Teacher" is a game where players share what made their favorite English teacher special. They talk about their teaching style, sense of humor, and memorable lessons. It's a fun way to reminisce about the positive impact teachers have on our language learning journey.`,
+  },
+  {
+    id: 5,
+    title_game1: `Guess Countries Following Flags`,
+    desc1: `"Guess Countries Following Flags" is a game where players identify countries based on their flags. One player displays a flag, and the others guess the corresponding country. It's a fun way to test geography knowledge and learn about different countries' flags.`,
+  },
+];
