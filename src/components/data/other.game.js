22f6acbd140a41ee933cd70afcd83994bce67d52
TemurEShtemirov <114666195+TemurEShtemirov@users.noreply.github.com>
2024-03-03 19:42:29 +0500

    Update other.game.js

--- src/components/data/other.game.js
+++ src/components/data/other.game.js
@@ -21,17 +21,17 @@ export const OtherGame = [
     id: 3,
     title_game1: `Describe Your Self`,
     desc1: `
-  "Describe Yourself" is a reflective activity where players articulate characteristics, interests, and experiences that define who they are as individuals. Each player takes turns sharing insights about themselves, such as personality traits, hobbies, aspirations, and memorable moments from their lives. The goal is to provide others with a deeper understanding of oneself, fostering connection and empathy within the group. It's an opportunity for self-expression and self-discovery, encouraging players to embrace their uniqueness and celebrate their personal journey.
+  "Describe Yourself" is a reflective activity where players articulate characteristics, interests, and experiences that define who they are as individuals. Each player takes turns sharing insights about themselves, such as personality traits, hobbies, aspirations, and memorable moments from their lives.
     `,
   },
   {
     id: 4,
     title_game1: `Describe Your Best English Teacher`,
-    desc1: `"Describe Your Best English Teacher" is a game where players share what made their favorite English teacher special. They talk about their teaching style, sense of humor, and memorable lessons. It's a fun way to reminisce about the positive impact teachers have on our language learning journey.`,
+    desc1: `"Describe Your Best English Teacher" is a game where players share what made their favorite English teacher special. They talk about their teaching style, sense of humor, and memorable lessons. `,
   },
   {
     id: 5,
     title_game1: `Guess Countries Following Flags`,
-    desc1: `"Guess Countries Following Flags" is a game where players identify countries based on their flags. One player displays a flag, and the others guess the corresponding country. It's a fun way to test geography knowledge and learn about different countries' flags.`,
+    desc1: `"Guess Countries Following Flags" is a game where players identify countries based on their flags. One player displays a flag, and the others guess the corresponding country.`,
   },
 ];
